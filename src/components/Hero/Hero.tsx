11f7e07eb4e9ace731b653d0e34a9b264f31f1aa
vikas-panchal7 <panchalvikas292000@gmail.com>
2024-09-10 12:53:50 +0530

    #chnages

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -22,7 +22,7 @@ export function Hero() {
           <h3>Full Stack Developer</h3>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
-          <p className="small-resume">3+ Years of Experience</p>
+          <p className="small-resume">5+ Years of Experience</p>
         </ScrollAnimation>
 
 
